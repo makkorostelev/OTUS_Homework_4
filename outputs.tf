f6205244b3086c8b7b1843cc4f5b266e9c4f2650
makkorostelev <makkorostelev@yandex.ru>
2023-12-16 19:53:24 +0300

    first commit

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+/*output "nginx_ips" {
+  value = yandex_compute_instance.nginx[*].network_interface.0.nat_ip_address
+}
+*/
+output "lb_ip" {
+  value = yandex_vpc_address.custom_addr.external_ipv4_address[0].address
+}
